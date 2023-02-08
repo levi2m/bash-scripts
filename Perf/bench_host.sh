adf1ed601010df1d710933e64b7d4cd9fe0a4373
Levi Maia <55702618+levi2m@users.noreply.github.com>
2023-02-08 18:32:40 -0300

    container_id
    changing from container_name to container_id, as it is less likely to fail

--- Perf/bench_host.sh
+++ Perf/bench_host.sh
@@ -3,6 +3,9 @@
 # Define the output file
 OUTPUT_FILE="lxc_benchmark_$(date +%Y%m%d%H%M%S).txt"
 
+# Define the ID of the LXC container
+CONTAINER_ID="<container_id>"
+
 # Start benchmarking
 echo "Starting LXC benchmark, recording output for 10 seconds..."
 start_time=$(date +%s)
@@ -14,7 +17,7 @@ while [ $(date +%s) -lt $end_time ]; do
   current_time=$(date +%s)
 
   # Record the CPU and memory usage of the LXC container
-  lxc_stats=$(lxc info --no-humanize)
+  lxc_stats=$(pct exec $CONTAINER_ID -- bash -c "top -bn 1")
   echo "$current_time: $lxc_stats" >> $OUTPUT_FILE
   
   # Sleep for 1 second before recording the next output

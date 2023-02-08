1a441f336832276da72aa0fe40e873642e812bc2
Levi Maia <55702618+levi2m@users.noreply.github.com>
2023-02-08 17:02:14 -0300

    Create performance_gather.sh

---
+++ Perf/performance_gather.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Update the package repository
+sudo apt-get update
+
+# Install necessary tools
+sudo apt-get install -y iostat htop ping traceroute ffmpeg
+
+# Define the log file
+log_file="performance_test.log"
+
+# Check if the tools are installed
+if [ $(which iostat) ]; then
+  echo "iostat is installed."
+else
+  echo "iostat is not installed."
+fi
+
+if [ $(which htop) ]; then
+  echo "htop is installed."
+else
+  echo "htop is not installed."
+fi
+
+if [ $(which ping) ]; then
+  echo "ping is installed."
+else
+  echo "ping is not installed."
+fi
+
+if [ $(which traceroute) ]; then
+  echo "traceroute is installed."
+else
+  echo "traceroute is not installed."
+fi
+
+if [ $(which ffmpeg) ]; then
+  echo "ffmpeg is installed."
+else
+  echo "ffmpeg is not installed."
+fi
+
+# Test CPU usage
+echo "Testing CPU usage..."
+echo "CPU Usage Test" >> $log_file
+iostat -c 30 >> $log_file
+
+# Test RAM usage
+echo "Testing RAM usage..."
+echo "RAM Usage Test" >> $log_file
+free -m -s 30 >> $log_file
+
+# Test disk I/O
+echo "Testing disk I/O..."
+echo "Disk I/O Test" >> $log_file
+iostat -dx 30 >> $log_file
+
+# Test network performance
+echo "Testing network performance..."
+echo "Network Performance Test" >> $log_file
+ping -c 30 www.google.com >> $log_file
+traceroute www.google.com >> $log_file
+
+# Test video streaming performance
+echo "Testing video streaming performance..."
+echo "Video Streaming Performance Test" >> $log_file
+ffmpeg -i video.mp4 -f null /dev/null 2>&1 | awk '/frame=.*fps.*time.*bitrate/ {print $0}' >> $log_file
